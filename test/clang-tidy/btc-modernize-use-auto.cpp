1300006b41f54e595e0bb0629f3a207bb5b6f4c3
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-10-09 07:56:13 -0400

    Added initial test case

--- test/clang-tidy/btc-modernize-use-auto.cpp
+++ test/clang-tidy/btc-modernize-use-auto.cpp
@@ -1,8 +1,20 @@
-// RUN: %check_clang_tidy %s btc-modernize-use-auto %t
+// RUN: %check_clang_tidy %s btc-modernize-use-auto %t \
+// RUN:   -- -std=c++11 -frtti
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [btc-modernize-use-auto]
+class MyType {};
+
+class MyDerivedType : public MyType {};
+
+// FIXME: the replacement sometimes results in two consecutive spaces after
+// the word 'auto' (due to the presence of spaces at both sides of '*').
+void auto_new() {
+  MyType a;
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: use auto
+  // CHECK-FIXES: auto a = MyType{};
+
+  // Don't warn when 'auto' is already being used.
+  auto aut = MyType{};
+}
 
 // FIXME: Verify the applied fix.
 //   * Make the CHECK patterns specific enough and try to make verified lines
@@ -10,5 +22,3 @@ void f();
 //   * Use {{}} for regular expressions.
 // CHECK-FIXES: {{^}}void awesome_f();{{$}}
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
